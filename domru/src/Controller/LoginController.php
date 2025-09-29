87c2c482b1fd111537fc4b10ab36ea22ff86fed6
k.spirikhin <k.spirikhin@tbank.ru>
2025-09-29 12:47:14 +0500

    version 0.2.5

--- domru/src/Controller/LoginController.php
+++ domru/src/Controller/LoginController.php
@@ -9,6 +9,7 @@ use Exception;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/login", name="login_");
